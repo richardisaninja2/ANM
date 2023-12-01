65aa63b5fa5e60a00295199b468e72d43b65a238
richard murrray <murray571a@gmail.com>
2023-11-30 19:49:44 -0600

    added new footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,15 @@ import { NavLink } from "react-router-dom";
 
 export default function Footer(){
     return(
-        <div className="footerCont">          
-                <a href="https://unscriptedphotographers.com/anmvisions"><h2>Booking <span></span></h2></a>
-                <NavLink to={'/'}><h1 className="">ANM</h1></NavLink>
-                <NavLink to="/portfolio"><h2>Portfolio<span></span></h2></NavLink>
+        <div>
+            <div className="footerCont">
+                    <a href="https://www.instagram.com/anmvisions/?igshid=YTQwZjQ0NmI0OA%3D%3D"><h2>Instagram <span></span></h2></a>       
+                    <a href="https://unscriptedphotographers.com/anmvisions"><h2>Booking <span></span></h2></a>
+                    <NavLink to="/portfolio"><h2>Portfolio <span></span></h2></NavLink>
+            </div>
+            <div className="footerCont">
+                <NavLink to={'/'}><h1 className="anmBig">ANM<br/>Photography</h1></NavLink>
+            </div>
         </div>
     )
 }
